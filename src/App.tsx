377f084228ad62818752ec039b8430ba5500ecde
s.riache <s.riache@cyberdian.fr>
2025-04-17 19:02:50 +0200

    fix(auth): fix cross platform auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "./redux/store";
+import { AppDispatch, RootState } from "./redux/store";
 import { Navigate, Outlet, Route, Routes } from "react-router-dom";
 import Auth from "./pages/Auth";
 import Home from "./components/Home";
 import { useEffect } from "react";
 import Cookies from "js-cookie";
 import { signInSuccess } from "./redux/auth/auth-slice";
+import { fetchCurrentUser } from "./utils/api/user-api";
 
 const ProtectedRoute = ({ isAuthenticated }: { isAuthenticated: boolean }) => {
   return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
@@ -15,13 +16,14 @@ const ProtectedRoute = ({ isAuthenticated }: { isAuthenticated: boolean }) => {
 const App = () => {
   const { accessToken } = useSelector((state: RootState) => state.auth);
   const { currentUser } = useSelector((state: RootState) => state.user);
-  const dispatch = useDispatch();
+  const dispatch = useDispatch<AppDispatch>();
   useEffect(() => {
     // Read the tokens from the shared cookies
     const accessToken = Cookies.get("accessToken");
     const refreshToken = Cookies.get("refreshToken");
-
     if (accessToken) {
+      console.log({ accessToken });
+      dispatch(fetchCurrentUser());
       dispatch(
         signInSuccess({ accessToken, refreshToken: refreshToken || null })
       );
